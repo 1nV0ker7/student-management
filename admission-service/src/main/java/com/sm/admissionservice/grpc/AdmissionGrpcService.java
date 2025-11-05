fe0c23e0f395bf18648e05056c1a193dc52b55de
1nV0ker7 <om.bayas.max@gmail.com>
2025-11-05 15:37:43 +0530

    GRPC service implemented

--- admission-service/src/main/java/com/sm/admissionservice/grpc/AdmissionGrpcService.java
+++ admission-service/src/main/java/com/sm/admissionservice/grpc/AdmissionGrpcService.java
@@ -4,18 +4,24 @@ import admission.AdmissionResponse;
 import admission.AdmissionServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class AdmissionGrpcService extends AdmissionServiceGrpc.AdmissionServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(AdmissionGrpcService.class);
+
     @Override
     public void createAdmissionAccount(
             admission.AdmissionRequest admissionRequest,
             StreamObserver<AdmissionResponse> responseObserver) {
+        log.info("createAdmissionAccount request received : {}", admissionRequest.toString());
+        AdmissionResponse response = AdmissionResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
 
-        
-
-
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
-
-
 }
